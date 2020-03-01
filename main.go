4133f5cc0dae103d6f19ce2a5f039f2328ae1b68
zjbztianya <spcjv5@gmail.com>
2020-03-01 18:27:00 +0800

    add bootstrap and navbar

--- main.go
+++ main.go
@@ -11,21 +11,21 @@ var contactView *views.View
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := homeView.Template.Execute(w, nil); err != nil {
+	if err := homeView.Template.ExecuteTemplate(w, homeView.Layout, nil); err != nil {
 		panic(err)
 	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := contactView.Template.Execute(w, nil); err != nil {
+	if err := contactView.Template.ExecuteTemplate(w, contactView.Layout, nil); err != nil {
 		panic(err)
 	}
 }
 
 func main() {
-	homeView = views.NewView("views/home.gohtml")
-	contactView = views.NewView("views/contact.gohtml")
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
--- views/view.go
+++ views/view.go
@@ -4,13 +4,17 @@ import "html/template"
 
 type View struct {
 	Template *template.Template
+	Layout   string
 }
 
-func NewView(files ...string) *View {
-	files = append(files, "views/layouts/footer.gohtml")
+func NewView(layout string, files ...string) *View {
+	files = append(files,
+		"views/layouts/footer.gohtml",
+		"views/layouts/navbar.gohtml",
+		"views/layouts/bootstrap.gohtml")
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
 	}
-	return &View{t}
+	return &View{t, layout}
 }
