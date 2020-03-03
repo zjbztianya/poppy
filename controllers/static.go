aaffd53817b17e3192fdb3b433aa6aaeefd37b38
zjbztianya <zjbztianya@163.com>
2020-03-04 01:10:37 +0800

    add FAQ pages

--- controllers/static.go
+++ controllers/static.go
@@ -5,12 +5,16 @@ import "github.com/zjbztianya/poppy/views"
 type Static struct {
 	Home    *views.View
 	Contact *views.View
+	FAQ     *views.View
 }
 
 func NewStatic() *Static {
-	return &Static{views.NewView(
-		"bootstrap", "static/home"),
+	return &Static{
+		views.NewView(
+			"bootstrap", "static/home"),
 		views.NewView(
 			"bootstrap", "static/contact"),
+		views.NewView(
+			"bootstrap", "static/faq"),
 	}
 }
